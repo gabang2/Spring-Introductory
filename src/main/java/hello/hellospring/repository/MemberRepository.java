09d925ff94a3169b43a09122b1516af0b68a10aa
gabang2 <offbeat1020@naver.com>
2023-08-09 17:21:50 +0900

    회원 서비스 개발

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,7 +6,7 @@ import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberInterface {
+public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long Id);
     Optional<Member> findByName(String name);
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -66,5 +66,4 @@ class MemoryMemberRepositoryTest {
         List<Member> result = repository.findAll();
         assertThat(result.size()).isEqualTo(2);
     }
-
 }
