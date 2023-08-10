037aacb629fbf949f6c5ae738903577251f91a2c
gabang2 <offbeat1020@naver.com>
2023-08-10 13:33:45 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller // spring container에 MemberController를 넣어서 관리 -> spring bean이 관리된다고 할 수 있음
+public class MemberController {
+
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
