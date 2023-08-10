b7567345b9f05cbaa5d299fb855e8c466614e921
gabang2 <offbeat1020@naver.com>
2023-08-10 13:56:25 +0900

    자바 코드로 직접 스프링 빈 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Controller;
 @Controller // spring container에 MemberController를 넣어서 관리 -> spring bean이 관리된다고 할 수 있음
 public class MemberController {
 
-
     private final MemberService memberService;
-
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,15 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
     private final MemberRepository memberRepository;
 
     /*
     회원가입
     */
-
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
