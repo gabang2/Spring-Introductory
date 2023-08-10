0ac003b6d64656ac384358ae3096d0c720160baa
gabang2 <offbeat1020@naver.com>
2023-08-10 15:26:35 +0900

    h2 설치 및 ddl 저장

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "home";
     }
-
-
-
 }
