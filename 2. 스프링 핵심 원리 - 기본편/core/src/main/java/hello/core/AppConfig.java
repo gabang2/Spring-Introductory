c8271e72f97650929c1d11fdb32be36fab06bf80
gabang2 <offbeat1020@naver.com>
2023-08-18 22:17:28 +0900

    스프링 핵심원리 이해 2 수강 완려

--- 2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,18 +1,36 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // 환경 구성은 AppConfig가 다 한다.
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
