af735c7b3172f60bc0e39af82a51ed2bc382c6f6
gabang2 <offbeat1020@naver.com>
2023-08-18 21:01:10 +0900

    관심사의 분리

--- 2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(long memberId, String itemName, int itemPrice) {
