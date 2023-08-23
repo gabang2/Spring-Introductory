90f7f777e4082b424843f41e71d78ede43f6730b
gabang2 <offbeat1020@naver.com>
2023-08-23 14:45:41 +0900

    회원 레포지토리 개발

--- 3. JPA활용 - 1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 3. JPA활용 - 1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,18 +18,34 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    //==연관관계 편의 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
