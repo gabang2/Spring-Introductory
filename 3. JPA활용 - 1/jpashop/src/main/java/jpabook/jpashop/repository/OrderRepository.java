8204ef98aa8585a8684042839f97b56aa5f79ea3
gabang2 <offbeat1020@naver.com>
2023-08-24 14:08:41 +0900

    주문 레포지터리 개발

---
+++ 3. JPA활용 - 1/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    // save
+    @Transactional
+    public void save(Order order) {
+        if (order.getId() == null) {
+            em.persist(order);
+        } else {
+            em.merge(order);
+        }
+    }
+
+    // findOne
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    // findAll <- orderSearch 가 있어야 함.
+//    public List<Order> findAll() {
+//        return em.createQuery("select o from Order o", Order.class)
+//                .getResultList();
+//    }
+}
